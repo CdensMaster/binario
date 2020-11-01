d1af23fd115c5370b8f456208dd8115d6a12a00e
CdensMaster <73200215+CdensMaster@users.noreply.github.com>
2020-11-01 10:38:11 -0500

    Add files via upload

---
+++ clase.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formatos;
+
+/**
+ *
+ * @author Denilson
+ */
+public class clase {
+    String signo,exponente,matisa,procedimiento,complemento;
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    
+    public String getSigno() {
+        return signo;
+    }
+
+    public void setSigno(String signo) {
+        this.signo = signo;
+    }
+
+    public String getExponente() {
+        return exponente;
+    }
+
+    public void setExponente(String exponente) {
+        this.exponente = exponente;
+    }
+
+    public String getMatisa() {
+        return matisa;
+    }
+
+    public void setMatisa(String matisa) {
+        this.matisa = matisa;
+    }
+
+    public String getProcedimiento() {
+        return procedimiento;
+    }
+
+    public void setProcedimiento(String procedimiento) {
+        this.procedimiento = procedimiento;
+    }
+    
+    
+}
